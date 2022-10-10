0f7b85a4d5bed597f4ad3a354af128204b1930ca
Soumyadip-62 <panditsoumya35@gmail.com>
2022-10-10 20:47:42 +0530

    pwa

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const withPWA = require("next-pwa")({
   // runtimeCaching,
   register: true,
   skipWaiting: true,
-  disable: process.env.NODE_ENV === "development",
+  // disable: process.env.NODE_ENV === "development",
 });
 // const runtimeCaching = require("next-pwa/cache");
 module.exports = withPWA({
